e36dcf6363e9b7061d867ec5535471935fcce378
Mehul Sathavara <mehulsathavara17@gmail.com>
2023-05-17 02:17:52 +0530

    First Commit.

---
+++ app.js
@@ -0,0 +1,77 @@
+//jshint esversion:6    
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const { subscribe } = require("diagnostics_channel");
+const https = require("https");
+const { callbackify } = require("util");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/" , function(req,res) {
+    
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address : email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+            }
+        }
+        ]
+    } 
+
+    const jsonData = JSON.stringify(data); //It make a string of that data ({"email_address":"mehulsathavara17@gmail.com","status":"subscribed"}).
+
+    const url = "https://us11.api.mailchimp.com/3.0/lists/b607b5bd6f";
+
+    const options = {
+        method: "post",
+        auth: "mehul1:d07703e91f516fa6433413077a7cad5b-us11"
+    }
+
+    const request = https.request(url, options, function(response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data) {
+            console.log(JSON.parse(data)); 
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("server is running in 3000.");
+});
+
+//API key
+//d07703e91f516fa6433413077a7cad5b-us11
+
+//audience ID
+//b607b5bd6f
